aa511327a93083dbbfdf89c5d06085f89a6cb34a
boykoc <codywboyko@gmail.com>
2019-04-12 20:18:59 +0000

    Task: Fix typos and add placeholder blueprints.

--- ckanext/adal/plugin.py
+++ ckanext/adal/plugin.py
@@ -1,6 +1,34 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from flask import Blueprint
+from flask import render_template_string
+
+def adal_login():
+    html = u'''<!DOCTYPE html>
+<html>
+    <head>
+        <title>ADAL Login</title>
+    </head>
+    <body>
+        Login.
+    </body>
+</html>'''
+
+    return render_template_string(html)
+
+def get_a_token():
+    html = u'''<!DOCTYPE html>
+<html>
+    <head>
+        <title>getAToken</title>
+    </head>
+    <body>
+        Token.
+    </body>
+</html>'''
+
+    return render_template_string(html)
 
 class AdalPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -30,6 +58,14 @@ class AdalPlugin(plugins.SingletonPlugin):
 
     # IBlueprints
 
-    def get_blueprints(self):
-        pass
+    def get_blueprint(self):
+        blueprint = Blueprint(self.name, self.__module__)
+        rules = [
+            ('/adal/login', 'adal_login', adal_login),
+            ('/adal/login/getAToken/', 'get_a_token', get_a_token)
+        ]
 
+        for rule in rules:
+            blueprint.add_url_rule(*rule)
+
+        return blueprint
